fda894edbb0a29892e55d8412ca923910552958f
moobeck <moritzbeck@mail.de>
2025-05-14 14:17:18 +0200

    Add cross validation config

---
+++ src/configurations/cross_validation.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class CrossValidationConfig:
+    """
+    A dataclass to store the time series cross-validation configuration.
+    """
+
+    cv_windows: int = 5
+    step_size: int = 14
