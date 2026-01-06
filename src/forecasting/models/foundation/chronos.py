54fd2973c7cb39847b64677fe923ca5e1f8892f1
moobeck <moritzbeck@mail.de>
2026-01-06 10:11:25 +0100

    Support CUDA device mapping during  Chronos initialization

--- src/forecasting/models/foundation/chronos.py
+++ src/forecasting/models/foundation/chronos.py
@@ -72,6 +72,7 @@ class Chronos(Forecaster):
         self.alias = alias
         self.pipeline = Chronos2Pipeline.from_pretrained(
             self.repo_id,
+            device_map="cuda"
         )
         self.futr_exog_list = futr_exog_list if futr_exog_list is not None else []
         self.hist_exog_list = hist_exog_list if hist_exog_list is not None else []
